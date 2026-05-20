0deba46dee4218137cfe3f3cc761c3ba3fcd51c4
miguel belmonte granados <miguel.belmonte@softtek.com>
2026-05-20 19:32:55 +0200

    chore: improve comment

--- thermia-front/src/environments/environment.prod.ts
+++ thermia-front/src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   apiUrl: 'https://themiabackend.cvbooster.es',
-  apiAuthToken: 'apikey_1234567890abcdef',
+  apiAuthToken: 'apikey_1234567890abcdef', // mejora: <- cargar de fuera, .env tampoco es seguro, acaba en el bundle
 };
